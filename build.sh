db462a08872ec19565552881f887292106ab5a08
Shaun Crampton <shaun@cantab.net>
2018-01-29 22:07:57 +0000

    Use a release instead of building from source.

--- build.sh
+++ build.sh
@@ -2,24 +2,33 @@
 
 set -e
 
-PACKAGES="git make autoconf automake libtool unzip"
+PACKAGES="git unzip"
 
 apt-get update
 apt-get install -y $PACKAGES
 
 # Install protoc.
-git clone https://github.com/google/protobuf -b $PROTOBUF_TAG --depth 1
-cd ./protobuf
-./autogen.sh
-./configure --prefix=/usr
-make -j 3
-make check
-make install
-cd ..
-rm -rf ./protobuf
+#git clone https://github.com/google/protobuf -b $PROTOBUF_TAG --depth 1
+#cd ./protobuf
+#./autogen.sh
+#./configure --prefix=/usr
+#make -j 3
+#make check
+#make install
+#cd ..
+#rm -rf ./protobuf
+
+wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
+unzip protoc-3.5.1-linux-x86_64.zip -d /usr/
 
 # Install gogo, an optimised fork of the Golang generators.
-go get github.com/gogo/protobuf/proto \
+mkdir -p "$GOPATH/src/github.com/gogo"
+cd "$GOPATH/src/github.com/gogo"
+git clone https://github.com/gogo/protobuf.git
+cd protobuf
+git checkout v0.5
+
+go install \
        github.com/gogo/protobuf/protoc-gen-gogo \
        github.com/gogo/protobuf/gogoproto \
        github.com/gogo/protobuf/protoc-gen-gogofast \
